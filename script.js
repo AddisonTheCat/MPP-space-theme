3511f8e512c7336b3317b5b62b5958012007fff4
Addison <79936655+AddisonTheCat@users.noreply.github.com>
2021-10-30 23:20:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,6 @@ var a=(new Date).toLocaleTimeString();setInterval(function(){a!==(new Date).toLo
 for (let i = 0; i < 10; i = i + 0.5) {setTimeout(function() {eval('Array.from(document.querySelectorAll("*")).forEach(el => {$(el).css({"border-radius": "' + i + 'px", "-webkit-border-radius": "' + i + 'px", "-moz-border-radius": "' + i + 'px" })})')}, i)}
 setInterval(() => {
 eval('Array.from(document.querySelectorAll("*")).forEach(el => {$(el).css({"border-radius": "' + i + 'px", "-webkit-border-radius": "' + i + 'px", "-moz-border-radius": "' + i + 'px" })})')
-$("#room").css({background: "rgba(0, 0, 0, 0.5)" });
-$("#room .more").css({background: "rgba(0, 0, 0, 0.5)"});
-$("#room .expand").css({background: "rgba(0, 0, 0, 0.5)"});
-$("#room .more .info:hover").css({background: "rgba(0, 0, 0, 0.5)"});
-$("#room .more .new").css({background: "rgba(0, 0, 0, 0.5)"});
-$("#room .more").css({background: "rgba(0, 0, 0, 0.5)"});
-$("#room .more .new:hover").css({background: "rgba(0, 0, 0, 0.5);"});
-$(".ugly-button").css({background: "rgba(0, 0, 0, 0)", "border-radius": "8px", "-webkit-border-radius": "8px", "-moz-border-radius": "8px" });
-$("#room .more .new:hover").css({background: "rgba(0, 0, 0, 0.5)"});
-$("#room .more .new:hover").css({background: "rgba(0, 0, 0, 0.5)"});
-$(".notification-body").css({background: "rgba(0, 0, 0, 0.5)", "backdrop-filter": "blur(2px)"});
-$(".notification .x:hover").css({background: "rgba(0, 0, 0, 0.5)"});
-$(".notification-body").css({background: "rgba(0, 0, 0, 0.5)", "backdrop-filter": "blur(2px)"});
-$(".ugly-button .stuck").css({background: "rgba(0, 0, 0, 0.5)"});
 document.body.style = `background-image: url('https://i.ibb.co/5WztDgx/space-earth.jpg'); background-position: 25% 75%; background-size: cover; backdrop-effect: blur(2px);`
 }, 1);
 class ChatBuffer {
