461c089465271ef512e88cad02b000f59caff396
Addison <79936655+AddisonTheCat@users.noreply.github.com>
2021-11-06 16:53:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var a=(new Date).toLocaleTimeString();setInterval(function(){a!==(new Date).toLocaleTimeString()&&(a=(new Date).toLocaleTimeString(),setTimeout(function(){document.getElementById("timebtn").childNodes[0].nodeValue=(new Date).toLocaleTimeString()},0))},0);$("body").append('<div id="timebtn" class="ugly-button" style="position: fixed; bottom: 17.5px !important; left: 1120px !important; z-index: 500;">0:0:0 PM/AM</div>');$("#timebtn").on("click",function(b){MPP.chat.send((new Date).toLocaleTimeString())});
 setInterval(() => {
-setTimeout(() => {Array.from(document.querySelectorAll("*")).forEach(el=> {$(el).css({"border-radius": "10px", "-webkit-border-radius": "10px", "-moz-border-radius": "10px" })})
-}, 3000);
+setTimeout(() => {Array.from(document.querySelectorAll("*")).forEach(el=> {$(el).css({"border-radius": "10px", "-webkit-border-radius": "10px", "-moz-border-radius": "10px" })})}, 3000);}, 1);
+function update(){
 $("#room").css({background: "rgba(0, 0, 0, 0.5)" });
 $("#room .more").css({background: "rgba(0, 0, 0, 0.5)"});
 $("#room .expand").css({background: "rgba(0, 0, 0, 0.5)"});
@@ -17,5 +17,6 @@ $(".notification .x:hover").css({background: "rgba(0, 0, 0, 0.5)"});
 $(".notification-body").css({background: "rgba(0, 0, 0, 0.5)", "backdrop-filter": "blur(2px)"});
 $(".ugly-button .stuck").css({background: "rgba(0, 0, 0, 0.5)"});
 document.body.style = `background-image: url('https://i.ibb.co/5WztDgx/space-earth.jpg'); background-position: 25% 75%; background-size: cover; backdrop-effect: blur(2px);`
-}, 100);
+}
+MPP.client.on(function(){update})
 for (let i = 0; i < 10; i = i + 0.5) {setTimeout(function() {eval('Array.from(document.querySelectorAll("*")).forEach(el=> {$(el).css({"border-radius": "' + i + 'px", "-webkit-border-radius": "' + i + 'px", "-moz-border-radius": "' + i + 'px" })})')}, i)}
