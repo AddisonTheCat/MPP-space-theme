4efa657564ae3f6cf7cf97e87f185759439ce80f
Addison <79936655+AddisonTheCat@users.noreply.github.com>
2021-11-06 16:44:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var a=(new Date).toLocaleTimeString();setInterval(function(){a!==(new Date).toLocaleTimeString()&&(a=(new Date).toLocaleTimeString(),setTimeout(function(){document.getElementById("timebtn").childNodes[0].nodeValue=(new Date).toLocaleTimeString()},0))},0);$("body").append('<div id="timebtn" class="ugly-button" style="position: fixed; bottom: 17.5px !important; left: 1120px !important; z-index: 500;">0:0:0 PM/AM</div>');$("#timebtn").on("click",function(b){MPP.chat.send((new Date).toLocaleTimeString())});
-for (let i = 0; i < 10; i = i + 0.5) {setTimeout(function() {eval('clearInterval(interval);var interval=setInterval(() => {\nArray.from(document.querySelectorAll("*")).forEach(el=> {$(el).css({"border-radius": "' + i + 'px", "-webkit-border-radius": "' + i + 'px", "-moz-border-radius": "' + i + 'px" })})}, 1);')}, i)}
+for (let i = 0; i < 10; i = i + 0.5) {setTimeout(function() {eval('Array.from(document.querySelectorAll("*")).forEach(el=> {$(el).css({"border-radius": "' + i + 'px", "-webkit-border-radius": "' + i + 'px", "-moz-border-radius": "' + i + 'px" })})')}, i)}
 setInterval(() => {
+setTimeout(() => {Array.from(document.querySelectorAll("*")).forEach(el=> {$(el).css({"border-radius": "10px", "-webkit-border-radius": "10px", "-moz-border-radius": "10px" })})
+}, 3000);
 $("#room").css({background: "rgba(0, 0, 0, 0.5)" });
 $("#room .more").css({background: "rgba(0, 0, 0, 0.5)"});
 $("#room .expand").css({background: "rgba(0, 0, 0, 0.5)"});
